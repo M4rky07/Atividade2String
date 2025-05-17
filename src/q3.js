4d08a697fe18091ece37f3c926d8f49fad71edf4
M4rky07 <juniorgomes22919@gmail.com>
2025-05-17 15:24:59 -0300

    Fiz a página de Html/ css/ JavaScript

--- src/q3.js
+++ src/q3.js
@@ -2,9 +2,8 @@
 // Crie uma função que receba uma string e transforme em um array, onde cada elemento do array é uma palavra da string.
 
 function TransformandoEmArray(str){ //CRiei uma função que recebe uma string
-    return str.split(' ') // O metodo split divide a string em um array, onde cada elemento do array é uma palavra da string
+    return str.split('  ') // O metodo split divide a string em um array, onde cada elemento do array é uma palavra da string
 
 }
 
-console.log(TransformandoEmArray("Lucas, João, Maria, Pedro, Ana")); // O resultado esperado é ["Lucas,", "João,", "Maria,", "Pedro,", "Ana"]
-console.log(TransformandoEmArray("Lucas João Maria Pedro Ana")); // O resultado esperado é ["Lucas,", "João,", "Maria,", "Pedro,", "Ana"]
+console.log(TransformandoEmArray("Lucas, João, Maria, Pedro, Ana")); // O resultado esperado é ["Lucas,", "João,", "Maria,", "Pedro,", "Ana"]clear
--- src/q4.js
+++ src/q4.js
@@ -1,6 +1,9 @@
+// Exercicio 4
+
 function cortarInicio(frase, quantidade) {
-    return frase.substring(0, quantidade);
+    return frase.substring(0, quantidade); // O metodo substring retorna uma parte da string, começando do índice 0 até o índice quantidade
 }
 
-let resultado = cortarInicio("Júnior gomes", 6);
+let resultado = cortarInicio("Júnior gomes", 6); // junior gomes equivale ao parametro frase e 6 equivale ao parametro quantidade
+
 console.log(resultado); // O resultado esperado é "Júnior"
