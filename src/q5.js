470babea92c6e456fcdddc879512068d7c796aa1
M4rky07 <juniorgomes22919@gmail.com>
2025-05-17 18:20:33 -0300

    Criei uma função que abrevia o ultimo nome do usúario.

--- src/q5.js
+++ src/q5.js
@@ -1,2 +1,14 @@
+function AbreviNome(nome) {
+  const ParteNome = nome.split(" "); // Divide o nome em partes usando o espaço como delimitador
+  // O método split() divide uma string em um array de substrings, usando o espaço como delimitador
+  if (ParteNome.length < 2) // Verifica se o nome tem menos de 2 partes
+    return nome; // Retorna o nome original se não tiver sobrenome
+  const PrimeiroNome = ParteNome[0]; // Pega o primeiro nome
+  const UltimoNomeIni = ParteNome[ParteNome.length - 1][0]; // Pega a primeira letra do último nome
+    // O método length retorna o número de elementos do array
+  return `${PrimeiroNome} ${UltimoNomeIni}.`; // Retorna o primeiro nome e a primeira letra do último nome com um ponto
+}
+
+console.log(AbreviNome("Rhavy Maia Guedes")); // O resultado esperado é "Rhavy G."
 
 
